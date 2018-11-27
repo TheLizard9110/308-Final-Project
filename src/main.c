7f0e86656562e5a93e25cafc90dbbee74c853585
Charles Tryban <chtryban@cougar.cs.ksu.edu>
2018-11-27 16:57:43 -0600

    adsfasdF

--- src/main.c
+++ src/main.c
@@ -4,9 +4,22 @@
 //  Copyright (c) 2018 NoUMedia
 //  hit f to pay royalties pl0x
 
+#include <stdio.h>
+
 #ifndef TEST
 int main(int argc, const char * argv[]) {
-    return 0;
+
+	if(argc >0) return 0;
+	printf("%s\n","Chose your board size, (S)mall, (M)edium, (L)arge");
+	//char input = scanf;
+
+	printf("%s\n","im just looking for a better way to get up out of bed instead of getting on the internet and checking out who hit me up fam");
+
+	
+	return 0;
 }
 #endif
+
+
+
 
